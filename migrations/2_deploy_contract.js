a559e432393464c072c0bd10baa35648fd78280a
Lior Rabin <lior@fuse.io>
2020-02-05 15:12:44 +0200

    deployment

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -2,12 +2,12 @@ require('dotenv').config()
 
 const DAIPointsToken = artifacts.require('./DAIPointsToken.sol')
 
-const { DAI_ADDRESS } = process.env
+const { DAI_ADDRESS, COMPOUND_ADDRESS } = process.env
 
 module.exports = (deployer, network, accounts) => {
   if (network !== 'test') {
     deployer.then(async function() {
-      let daiPointsToken = await DAIPointsToken.new(DAI_ADDRESS)
+      let daiPointsToken = await DAIPointsToken.new(DAI_ADDRESS, COMPOUND_ADDRESS)
       console.log(`DAIPointsToken: ${daiPointsToken.address}`)
     }).catch(function(error) {
       console.error(error)
