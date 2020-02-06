ba4312f326ef6a35b0d1c195928c8c5dac135fb1
Lior Rabin <lior@fuse.io>
2020-02-06 16:55:17 +0200

    reward amount in DAIp

--- oracle/src/utils/services.js
+++ oracle/src/utils/services.js
@@ -40,10 +40,14 @@ async function getReward () {
   const fee = toBN(await DAIp.methods.fee.call())
   logger.debug(`fee: ${fromWei(fee)}`)
 
-  let rewardAmount = grossWinnings.mul(decimals.sub(fee)).div(decimals)
-  logger.info(`rewardAmount: ${fromWei(rewardAmount)}`)
+  const daiRewardAmount = grossWinnings.mul(decimals.sub(fee)).div(decimals)
+  logger.debug(`daiRewardAmount: ${fromWei(daiRewardAmount)}`)
 
-  return rewardAmount
+  const rate = toBN(await DAIp.methods.daiToDaipConversionRate.call())
+  const daipRewardAmount = daiRewardAmount.mul(rate)
+  logger.info(`daipRewardAmount: ${fromWei(daipRewardAmount)}`)
+
+  return daipRewardAmount
 }
 
 async function selectWinner () {
